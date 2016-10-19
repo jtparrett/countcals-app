0b9a5b12098aa8018f1b73e7f9089405c570d7f5
jtparrett <jtparrett@me.com>
2016-10-19 20:12:10 +0100

    item emojis

--- ui/javascripts/_components/fooditem.js
+++ ui/javascripts/_components/fooditem.js
@@ -1,10 +1,27 @@
 var FoodItem = React.createClass({
+  getInitialState: function(){
+    let emojis = {
+      '😄': 300,
+      '😁': 500,
+      '😐': 700,
+      '😵': 1000
+    };
+
+    let emoji = Object.keys(emojis)[0];
+    Object.keys(emojis).map(function(key){
+      if(this.props.data.calories >= emojis[key]){
+        emoji = key;
+      }
+    }.bind(this));
+
+    return { emoji: emoji };
+  },
   render: function(){
     return (
       <article className="fooditem">
         <h1 className="fooditem__title">{this.props.data.name}</h1>
         <div>
-          <p className="emoji">😊</p>
+          <p className="emoji">{this.state.emoji}</p>
         </div>
         <div>
           <p className="fooditem__calories">{this.props.data.calories}<small>cals</small></p>
